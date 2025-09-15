8980b0b3596864d5dda0d613a81018286729536d
MohammadEiid <mohammad.a.eiid@gmail.com>
2025-09-15 21:36:26 +0300

    product

--- backend/controllers/ProductController.ts
+++ backend/controllers/ProductController.ts
@@ -1,7 +1,37 @@
 const express = require("express");
 const pool = require("../models/db");
 
-// GET /products  -> list all products
+
+
+// 1. **Create*
+const createProduct = async (req: any, res: any): Promise<void> => {
+  const { title, description, image_url, category_id, price, user_id, is_feature }: { 
+    title: string; 
+    description: string; 
+    image_url: string; 
+    category_id: number; 
+    price: number; 
+    user_id: number; 
+    is_feature?: boolean; 
+  } = req.body;
+
+  try {
+    const result = await pool.query(
+      "INSERT INTO products (title, description, image_url, category_id, price, user_id, is_feature) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+      [title, description, image_url, category_id, price, user_id, is_feature || false] // الافتراضي يكون false إذا لم يتم تحديده
+    );
+    
+    res.status(201).json({
+      success: true,
+      message: "Product created successfully",
+      product: result.rows, 
+    });
+  } catch (err: any) {
+    console.error("Error creating product:", err.message);
+    res.status(500).json({ success: false, error: "server error" });
+  }
+};
+//2 -GET /products  -> list all products
 const getAllProduct = async (req: any, res: any) => {
 try{
 const result = await pool.query ('SELECT * FROM products WHERE is_deleted = 0;')
@@ -16,4 +46,6 @@ catch (err:any){
 }
 };
 
-module.exports = { getAllProduct };
+
+
+module.exports = { createProduct,getAllProduct };
--- backend/routes/productRoute.ts
+++ backend/routes/productRoute.ts
@@ -1,8 +1,16 @@
 const express = require("express");
 const  productRouter= express.Router();
-const {getAllProduct}=require("../controllers/ProductController")
+const {getAllProduct, createProduct}=require("../controllers/ProductController")
 
+
+// create path ===> POST http://localhost:5000/products
+productRouter.post("/", createProduct);
+
+// get path ===> GET http://localhost:5000/products
 productRouter.get("/",getAllProduct);
+
+
+
 
 module.exports = productRouter;
 
