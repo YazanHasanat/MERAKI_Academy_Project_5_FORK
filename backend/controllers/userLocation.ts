993c3de041ebff32da23c229493ad74719ee505e
YazanHasanat <yazenhasanat@gmail.com>
2025-09-15 22:59:48 +0300

    get location function

--- backend/controllers/userLocation.ts
+++ backend/controllers/userLocation.ts
@@ -1,6 +1,7 @@
 const pool = require("../models/db");
+import type { Request, Response } from "express";
 
-const createLocation = async (req: any, res: any) => {
+const createLocation = async (req: Request, res: Response) => {
   try {
     const { user_id, address, latitude, longitude, is_deleted } = req.body;
     const result = await pool.query(
@@ -21,4 +22,19 @@ const createLocation = async (req: any, res: any) => {
   }
 };
 
-module.exports = { createLocation };
+const getLocationById=async(req:Request,res:Response)=>{
+  const {user_id}=req.body
+  try{
+    const result=await pool.query("SELECT * FROM user_locations WHERE user_id=$1",[user_id])
+    res.status(200).json({
+      success: true,
+      location: result.rows,
+    });
+  }
+  catch(err:any){
+    console.error("Error fetching orders:", err.message);
+    res.status(500).json({ success: false, error: "Internal server error" });
+  }
+}
+
+module.exports = { createLocation,getLocationById };
