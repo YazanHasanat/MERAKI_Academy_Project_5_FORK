1d76c989a7f3c67364db18a505b9d0fe6722080f
YazanHasanat <yazenhasanat@gmail.com>
2025-09-15 21:52:58 +0300

    test

--- backend/controllers/ProductController.ts
+++ backend/controllers/ProductController.ts
@@ -1,8 +1,10 @@
+import type e = require("express");
+
 const express = require("express");
 const pool = require("../models/db");
 
 // GET /products  -> list all products
-const getAllProduct = async (req: any, res: any) => {
+const getAllProduct = async (req: e.Response, res: any) => {
 try{
 const result = await pool.query ('SELECT * FROM products WHERE is_deleted = 0;')
     res.status(200).json({
@@ -15,5 +17,7 @@ catch (err:any){
     res.status(500).json({ success: false, error: "Internal server error" });
 }
 };
-
+const getlocation=(req:any,res:any)=>{
+    const {user_id}=req.body
+}
 module.exports = { getAllProduct };
