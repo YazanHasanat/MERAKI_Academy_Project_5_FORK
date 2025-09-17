c78ae12d3017eef075ab0db24af39eff80bf1d72
YazanHasanat <yazenhasanat@gmail.com>
2025-09-17 14:54:21 +0300

    add_redux

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { ReduxProvider } from "./lip/stroeProvider";
 
 
 
@@ -16,7 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <ReduxProvider>
         {children}
+        </ReduxProvider>
       </body>
     </html>
   );
