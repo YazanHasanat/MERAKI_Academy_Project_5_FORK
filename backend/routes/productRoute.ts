398a432924fb95ef0eaf984bf014e5a70c0ab7a0
YazanHasanat <yazenhasanat@gmail.com>
2025-09-17 15:54:25 +0300

    get_p_by_category

--- backend/routes/productRoute.ts
+++ backend/routes/productRoute.ts
@@ -1,13 +1,18 @@
 const express = require("express");
-const  productRouter= express.Router();
-const {getAllProduct, createProduct, updateProduct, softDeleteProduct}=require("../controllers/ProductController")
-
+const productRouter = express.Router();
+const {
+  getAllProduct,
+  createProduct,
+  updateProduct,
+  softDeleteProduct,
+  getProductsByCategory,
+} = require("../controllers/ProductController");
 
 // create path ===> POST http://localhost:5000/products
 productRouter.post("/", createProduct);
 
 // get path ===> GET http://localhost:5000/products
-productRouter.get("/",getAllProduct);
+productRouter.get("/", getAllProduct);
 
 // update  path ===> put http://localhost:5000/products/:id
 productRouter.put("/:id", updateProduct);
@@ -15,28 +20,9 @@ productRouter.put("/:id", updateProduct);
 // softDeleted path ===> delete http://localhost:5000/products/:id
 productRouter.delete("/:id", softDeleteProduct);
 
+productRouter.get(
+  "/:categoryId",
+  getProductsByCategory
+ // http://localhost:5000/products/:categoryId
+);
 module.exports = productRouter;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
