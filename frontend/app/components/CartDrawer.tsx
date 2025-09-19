e67219a7d1824ca08c16f3838d6d3c25528c7ea6
YazanHasanat <yazenhasanat@gmail.com>
2025-09-19 17:51:33 +0300

    dialog_location

--- frontend/app/components/CartDrawer.tsx
+++ frontend/app/components/CartDrawer.tsx
@@ -5,24 +5,80 @@ import Drawer from "@mui/material/Drawer";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+
 interface CartDrawerProps {
   open: boolean;
   onClose: () => void;
 }
 
 export default function CartDrawer({ open, onClose }: CartDrawerProps) {
-    
+  const [openDialog, setOpenDialog] = React.useState(false);
+
+  const handleDialogOpen = () => setOpenDialog(true);
+  const handleDialogClose = () => setOpenDialog(false);
+
   return (
-    <Drawer anchor="right" open={open} onClose={onClose}>
-      <Box sx={{ width: 350, p: 2 }}>
-        <Box sx={{ display: "flex", justifyContent: "space-between", mb: 2 }}>
-          <Typography variant="h6">Your Cart</Typography>
-          <Button onClick={onClose} sx={{ textTransform: "none" }}>
-            Close
-          </Button>
+    <>
+      <Drawer anchor="right" open={open} onClose={onClose}>
+        <Box
+          sx={{
+            width: 350,
+            p: 2,
+            display: "flex",
+            flexDirection: "column",
+            height: "100%",
+          }}
+        >
+          <Box sx={{ display: "flex", justifyContent: "space-between", mb: 2 }}>
+            <Typography variant="h6">Your Cart</Typography>
+            <Button onClick={onClose} sx={{ textTransform: "none" }}>
+              Close
+            </Button>
+          </Box>
+
+          <Box sx={{ flexGrow: 1 }}>
+            <Typography variant="body1">Cart is empty</Typography>
+          </Box>
+
+          <Box sx={{ mt: 2 }}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleDialogOpen}
+              fullWidth
+            >
+              Add Location
+            </Button>
+          </Box>
         </Box>
-        <Typography variant="body1">Cart is empty</Typography>
-      </Box>
-    </Drawer>
+      </Drawer>
+
+      {/* Dialog location*/}
+      <Dialog
+        open={openDialog}
+        onClose={handleDialogClose}
+        fullWidth
+        maxWidth="md"
+      >
+        <DialogTitle>Location</DialogTitle>
+        <DialogContent dividers>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18..."
+            width="100%"
+            height="400"
+            style={{ border: 0 }}
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDialogClose}>Close</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
