0e5529d665b207458c96c28856fc89bce2d41c5e
RaghadAlfayoumi <alfayoumii.raghad@gmail.com>
2025-09-20 00:50:36 +0300

    login

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,7 +1,54 @@
-const login = () => {
-  return (
-    <div>login</div>
-  );
-};
+"use client";
 
-export default login;
+import axios from "axios";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+
+ const Login=()=> {
+  const router = useRouter();
+
+  
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post("http://localhost:5000/users/login", {
+        email,
+        password,
+      });
+
+      console.log("login success", response.data);
+
+      
+      router.push("/"); 
+    } catch (error: any) {
+      console.error("login error", error.message);
+    }
+  };
+
+  return (
+    <>
+      <h2>Login</h2>
+
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleLogin}>Sign In</button>
+    </>
+  );
+}
+
+export default Login;
