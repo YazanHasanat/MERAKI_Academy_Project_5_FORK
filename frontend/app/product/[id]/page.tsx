0c2cc1bf4edc969ca29aa1f9c6b45751be8a0e05
MomenAbdullah <momenahmad.abdullah@gmail.com>
2025-09-20 18:06:10 +0300

    get product id

--- frontend/app/product/[id]/page.tsx
+++ frontend/app/product/[id]/page.tsx
@@ -1,14 +1,37 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { useRouter } from "next/router";
 import {
   Box,
   Typography,
   Card,
-  CardMedia,
   CardContent,
   CircularProgress,
   Button,
+  Chip,
+  Divider,
 } from "@mui/material";
 import axios from "axios";
+import { useParams, useRouter } from "next/navigation";
+
+interface Product {
+  id: number;
+  title: string;
+  description?: string;
+  image_urls: string[];
+  category_id?: number;
+  price: number;
+  is_feature: boolean;
+  created_at: string;
+  is_deleted: number;
+}
+
+const ProductPage = () => {
+  const router = useRouter();
+  const params = useParams();
+  const id = params.id as string;
+
+  const [product, setProduct] = useState<Product | null>(null);
+  const [loading, setLoading] = useState(true);
+
+}
