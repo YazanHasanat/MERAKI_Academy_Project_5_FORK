7450bd5f926d5da4bbed4ea78c16360d060264a7
YazanHasanat <yazenhasanat@gmail.com>
2025-09-20 22:56:36 +0300

    edite_user

--- backend/routes/usersRoute.ts
+++ backend/routes/usersRoute.ts
@@ -1,12 +1,12 @@
 const express = require("express");
 
-
-const {register,login,getAllUsers}=require("../controllers/usersController")
+const authentication=require("../middleware/authentication")
+const {register,login,getAllUsers,getUserById}=require("../controllers/usersController")
 const usersRouter = express.Router();
 usersRouter.post("/register",register)
 usersRouter.post("/login",login)
 usersRouter.get("/get",getAllUsers)
-
+usersRouter.get("/mypage",authentication,getUserById)
 
 
 
--- frontend/app/user/[id]/page.tsx
+++ frontend/app/user/[id]/page.tsx
@@ -1,6 +1,32 @@
+"use client";
+
+import axios from "axios";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
 const user = () => {
+  const { id } = useParams();
+  const [user,setuser]=useState([])
+  const getInformation=async()=>{
+    const res=await axios.get("http://localhost:5000/users/mypage",{
+      headers:{
+        Authorization:`Bearer ${localStorage.getItem("token")}`
+      }
+    })
+    setuser(res.data)
+    console.log(res.data);
+  }
+useEffect(()=>{
+  getInformation()
+},[])
+
   return (
-    <div>user</div>
+    <div>user
+      {user.map((ele)=>{
+        return(<div>
+        </div>)
+      })}
+    </div>
   );
 };
 
